37bcf89296b07de7590794dadc14ea1598a6776e
Semih Arda <ongul357@gmail.com>
2025-09-24 15:55:24 +0300

    new

--- frontend/app/(auth)/register/page.tsx
+++ frontend/app/(auth)/register/page.tsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 import { useState } from 'react'
 import { Eye, EyeOff, Mail, Lock, User, ArrowRight, Sparkles, Home } from 'lucide-react'
 import Image from 'next/image'
-import underwater from '@/images/underwater01.webp'
+import underwater from '@/images/küre.png'
 import Link from 'next/link'
 import spaceYouthIcon from '@/images/SpaceYouth.ico'
 
